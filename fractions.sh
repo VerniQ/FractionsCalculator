527e4f700734c0de1098ddc648a79e7f8c09c0f4
Verni <81250751+VerniQ@users.noreply.github.com>
2024-01-13 13:37:14 +0100

    Update fractions.sh

--- fractions.sh
+++ fractions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-gcd() {
-    # Ensure two arguments are provided
+#Skrypt Bash do programu napisanego w języku C, sposób działania większośći rzeczy jest taki sam jak w drugim programie. Pozostałe rzeczy zostaną wyjaśnione
+
+gcd() { #Największy wspólny dzielnik
     if [ "$#" -ne 2 ]; then
         echo "Invalid input"
         return 1
@@ -10,13 +11,11 @@ gcd() {
     a=$1
     b=$2
 
-    # Ensure both inputs are non-negative integers
     if [[ ! $a =~ ^[0-9]+$ || ! $b =~ ^[0-9]+$ ]]; then
         echo "Invalid input"
         return 1
     fi
 
-    # Euclidean algorithm for finding GCD
     while [ $b -ne 0 ]; do
         remainder=$((a % b))
         a=$b
@@ -26,99 +25,14 @@ gcd() {
     echo "$a"
 }
 
-abs() {
+abs() { #Język Bash nie posiada wbudowanej funkcji odpowiadającej za zwracanie wartości bezwzględnej, dlatego została stworzona.
     if [ $1 -lt 0 ]; then
         echo $((-$1))
     else
         echo $1
     fi
 }
-sum() {
-    a=$1
-    b=$2
-
-    c=$3
-    d=$4
-
-    numerator=$((a * d + c * b))
-    denominator=$((b * d))
-    printFraction $numerator $denominator
-}
-sub(){
-    a=$1
-    b=$2
-
-    c=$3
-    d=$4
-    numerator=$((a*d-c*d))
-    denominator=$((b*d))
-    printFraction $numerator $denominator
-}
-mul(){
-    a=$1
-    b=$2
-
-    c=$3
-    d=$4
-    numerator=$((a*c))
-    denominator=$((b*d))
-    printFraction $numerator $denominator
-}
-quo(){
-    a=$1
-    b=$2
-
-    c=$3
-    d=$4
-    numerator=$((a*d))
-    denominator=$((b*c))
-    printFraction $numerator $denominator
-
-}
-fractionOperation() {
-    a=$1
-    b=$2
-    c=$3
-    d=$4
-    operation=$5
-    printFraction $a $b
-    echo "$operation"
-    printFraction $c $d
-    echo "="
-
-    if [ $operation -eq 1 ]; then
-        sum $a $b $c $d
-    elif [ $operation -eq 2 ]; then
-        sub $a $b $c $d
-    elif [ $operation -eq 3 ]; then
-        mul $a $b $c $d
-    elif [ $operation -eq 4 ]; then
-        quo $a $b $c $d
-    fi
-}
-fractionOperationCase() {
-    a=$1
-    b=$2
-    c=$3
-    d=$4
-    operation=$5
-
-    case "$operation" in
-        +) 
-            sum "$a" "$b" "$c" "$d"
-            ;;
-        -)
-            sub "$a" "$b" "$c" "$d"
-            ;;  
-        %)
-            quo "$a" "$b" "$c" "$d"
-            ;;  
-        *)
-            mul "$a" "$b" "$c" "$d"
-            ;;
-    esac
-}
-printFraction() {
+printFraction() { #Funkcja wypisująca ułamki w postaci właściwej, schemat działania identyczny jak w przypadku drugiego programu.
     a=$1
     b=$2
     abs_a=$(abs $a)
@@ -126,21 +40,21 @@ printFraction() {
 
     if [ $b -eq 0 ]; then
         echo "NaN"
-        return 
+        return
     fi
 
     if [ $a -eq 0 ]; then
         echo "0"
-        return 
+        return
     fi
     if [ $a -eq $b ]; then
         echo "1"
-        return 
+        return
     fi
 
     gcd_temp=$(gcd $abs_a $abs_b)
-    if [ $((abs_a)) -lt $((abs_b)) ]; then
 
+    if [ $((abs_a)) -lt $((abs_b)) ]; then
         if [ "$gcd_temp" -ne 1 ]; then
             if [ $a -lt 0 ] || [ $b -lt 0 ]; then
                 if [ $a -lt 0 ] && [ $b -lt 0 ]; then
@@ -181,7 +95,7 @@ printFraction() {
         if [ "$gcd_temp" -ne 1 ]; then
             modulo=$((abs_a%abs_b))
             if [ $modulo -eq 0 ]; then
-                if [ $a -lt 0 ] || [ $b -lt 0 ]; then 
+                if [ $a -lt 0 ] || [ $b -lt 0 ]; then   
                     if [ $a -lt 0 ] && [ $b -lt 0 ]; then
                         temp=$(($abs_a/$abs_b))
                         echo "$temp"
@@ -191,7 +105,6 @@ printFraction() {
                         echo "-$temp"
                         return
                     fi
-
                 else
                     temp=$(($abs_a/$abs_b))
                     echo "$temp"
@@ -200,7 +113,7 @@ printFraction() {
             else
                 reduceNominator=$((abs_a / gcd_temp))
                 reduceDenominator=$((abs_b / gcd_temp))
-                temp=$((reduceNominator/reduceDenominator))        
+                temp=$((reduceNominator/reduceDenominator))          
                 temp2=$((reduceNominator%reduceDenominator))
                 if [ $a -lt 0 ] || [ $b -lt 0 ]; then
                     if [ $a -lt 0 ] && [ $b -lt 0 ]; then
@@ -215,10 +128,10 @@ printFraction() {
                     return
                 fi
             fi
-        else 
+        else   
             temp=$((abs_a/abs_b))
             temp2=$((abs_a%abs_b))
-            if [ $a -lt 0 ] || [ $b -lt 0 ]; then 
+            if [ $a -lt 0 ] || [ $b -lt 0 ]; then   
                 if [ $a -lt 0 ] && [ $b -lt 0 ]; then
                     echo "$temp $temp2/$abs_b"
                     return
@@ -233,18 +146,125 @@ printFraction() {
         fi
     fi
 }
-IFS='/' read -p "Podaj ulamek w formacie a/b: " numerator1 denominator1
-echo "Numerator: $numerator1"
-echo "Denominator: $denominator1"
 
-IFS='/' read -p "Podaj ulamek w formacie a/b: " numerator2 denominator2
-echo "Numerator: $numerator2"
-echo "Denominator: $denominator2"
+#Funkcje wykonujące działania na ułamkach, sposób działania identyczny jak w programie w języku C
 
-read -p "Podaj działanie do wykonania( +, -, *, %)" operation
-echo "Operation: $operation"
+sum() {
+    a=$1
+    b=$2
+    c=$3
+    d=$4
 
-fractionOperationCase $numerator1 $denominator1 $numerator2 $denominator2 $operation
+    numerator=$(((a * d) + (c * b)))
+    denominator=$((b * d))
 
+    printFraction $numerator $denominator
+}
 
+sub() {
+    a=$1
+    b=$2
+    c=$3
+    d=$4
 
+    numerator=$((a * d - c * b))
+    denominator=$((b * d))
+
+    printFraction $numerator $denominator
+}
+
+mul() {
+    a=$1
+    b=$2
+    c=$3
+    d=$4
+
+    numerator=$((a * c))
+    denominator=$((b * d))
+
+    printFraction $numerator $denominator
+}
+
+quo() {
+    a=$1
+    b=$2
+    c=$3
+    d=$4
+
+    numerator=$((a * d))
+    denominator=$((b * c))
+
+    printFraction $numerator $denominator
+}
+
+#Wykonywanie odpowiedniego działania na podstawie znaku wprowadzonego przez użytkowanika, sposób działania identyczny jak w programie w języku C
+
+fractionOperation() {
+    a=$1
+    b=$2
+    c=$3
+    d=$4
+    operation=$5
+    printFraction $a $b
+    echo "$operation"
+    printFraction $c $d
+    echo "="
+
+    case "$operation" in
+        +)
+            sum "$1" "$2" "$3" "$4"
+            ;;
+        -)
+            sub "$1" "$2" "$3" "$4"
+            ;;
+        /)
+            quo "$1" "$2" "$3" "$4"
+            ;;
+        *)
+            mul "$1" "$2" "$3" "$4"
+            ;;
+    esac
+}
+
+#Funkcje wczytujące ułamek oraz znak działania, sposób działania identyczny jak w języku C, program prosi o podanie wartości do momentu wprowadzenia ułamków w odpowienij formie oraz odpowiedniego znaku
+
+readFraction() {
+    while true; do
+        read -p "Podaj ulamek w formacie a/b: " input
+
+        if [[ "$input" =~ ^[0-9]+/[0-9]+$ ]]; then
+            IFS='/' read -r numerator denominator <<< "$input"
+            echo "$numerator $denominator"
+            break
+        else
+            echo "Błędny format. Podaj ulamek w formacie a/b."
+        fi
+    done
+}
+readOperation() {
+    local operation
+    while true; do
+        read -p "Wprowadz dzialanie (+, -, *, /): " operation
+        case $operation in
+            [+-/*]) break ;;
+            *) echo "Bledne dzialanie. Wprowadz ponownie." ;;
+        esac
+    done
+    echo "$operation"
+}
+
+#Poniższy kod odpowiada funkcji main() w języku C, wywoływane są tu funkcje.
+
+echo "Wprowadź pierwszy ułamek:"
+read -r numerator1 denominator1 <<< "$(readFraction)"
+
+echo "Wprowadź drugi ułamek:"
+read -r numerator2 denominator2 <<< "$(readFraction)"
+
+operation=$(readOperation)
+if ["$operation" -eq '/' ] &&  [$numerator1 -eq 0]; then
+    echo "Błąd! Dzielenie przez zero jest niewykonywalne!"
+    exit
+else
+    fractionOperation $numerator1 $denominator1 $numerator2 $denominator2 "$operation"
+fi
